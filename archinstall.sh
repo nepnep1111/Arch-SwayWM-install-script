c7dcd5daf4f190263bebc0dd11fb359b88d0c472
nepnep1111 <jmertens00@gmail.com>
2021-02-22 20:43:47 -0600

    Prepare for rewrite
    Overhaul allowing for variables and attempts to allow for adding more devices

--- archinstall.sh
+++ archinstall.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
+#---------------------------------------------btrfs install w.i.p
 #mount root btrfs partition
 
 #mount /dev/sda2 /mnt
@@ -28,29 +29,280 @@
 
 #pacstrap and stuff
 
-#----------------------------------------------------------
+#-----------------------------------------------------------------
 
 #install arch on x240 with gpt with root, boot partition, f2fs, refind/systemdboot, gpt, and zswap
 
 #format and mount root f2fs
-pacman -S f2fs-tools
-mkfs.f2fs -f -l root -O extra_attr,inode_checksum,sb_checksum,compression /dev/sda2
-mkdir /mnt
-mount -o compress_algorithm=zstd /dev/sda2 /mnt
+#pacman -S f2fs-tools
+#mkfs.f2fs -f -l root -O extra_attr,inode_checksum,sb_checksum,compression /dev/sda2
+#mkdir /mnt
+#mount -o compress_algorithm=zstd /dev/sda2 /mnt
 
 #mount boot fat32
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+#mkdir /mnt/boot
+#mount /dev/sda1 /mnt/boot
 
 #create swapfile
-dd if=/dev/zero of=/mnt/swapfile bs=1M count=16386
-chmod 600 /mnt/swapfile
-swapon /mnt/swapfile
+#dd if=/dev/zero of=/mnt/swapfile bs=1M count=16386
+#chmod 600 /mnt/swapfile
+#swapon /mnt/swapfile
 
 #install reflector and run to get mirrors
-pacman -S reflector
-reflector --country United States --48 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syu
+#pacman -S reflector
+#reflector --country United States --48 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+#pacman -Syu
 
 #pacstrap install
-pacstrap /mnt base linux linux-firmware nano man-db man-pages texinfo f2fs-tools btrfs-progs
+#pacstrap /mnt base linux linux-firmware nano man-db man-pages texinfo f2fs-tools btrfs-progs
+
+
+#Text attributes'
+bold='\033[1m'
+underscore='\033[4m'
+blink='\033[5m'
+reverseVideo='\033[7m'
+conceal='\033[8m'
+AO='\033[0m' # alloff
+
+#text color
+
+black='\033[30m'
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+blue='\033[34m'
+magenta='\033[35m'
+cyan='\033[36m'
+white='\033[37m'
+
+#background colors
+
+bblack='\033[40m'
+bred='\033[41m'
+bgreen='\033[42m'
+byellow='\033[43m'
+bblue='\033[44m'
+bmagenta='\033[45m'
+bcyan='\033[46m'
+bwhite='\033[47m'
+
+oldname="test"
+newname="testme"
+
+#archpackages
+archbase="base linux linux-firmware nano man-db man-pages texinfo f2fs-tools btrfs-progs"
+archsway=""
+archextra=""
+archdesktop=""
+archx240=""
+archgpdwinmax=""
+
+#MAIN MENU
+echo "
+${green}Main Menu${AO}
+1. Partition Drive
+2. Format Partitions
+3. Install Arch
+4. Arch-chroot/postinstall
+5. #
+88. Exit
+99. Shut Down"
+read select
+case $select in
+
+  1)
+    #Partition Drive MENU
+    echo -e "
+    ${yellow}Partition Drive${AO}
+    1. #
+    2. #
+    3. #
+    4. #
+    5. #
+    6. #
+    7. #
+    8. #
+    9. #
+    10. #
+    88. Return to Main Menu
+    99. Shutdown"
+    read PartitionDrive
+    case $PartitionDrive in
+
+      1) #
+        #
+        ;;
+      2) #
+        #
+        ;;
+      3) #
+        #
+        ;;
+      4) #
+        #
+        ;;
+      5) #
+        #
+        ;;
+      6) #
+        #
+        ;;
+      7) #
+        #
+      ;;
+      8) #
+        #
+      ;;
+      9) #
+        #
+      ;;
+      10) #
+        #
+      ;;
+      88) #return to main MENU
+      ./archinstall.sh
+      ;;
+      99) #Shutdown
+      poweroff
+      ;;
+    esac
+;;
+  2)
+
+      #Format Partitions MENU
+      echo "
+      ${yellow}Format Partitions${AO}
+      1. Format SATA SSD for F2FS
+      2. Format NVME SSD for F2FS
+      3. Format SATA BOOTorESP partition
+      4. Format NVME BootorESP parition
+      5. #
+      88. Return to main menu
+      99. Shutdown"
+      read formatpartitions
+      case $formatpartitions in
+
+        1)#Format SATA SSD for F2FS
+		echo "checking if f2fs-tools are installed"
+        pacman -S f2fs-tools
+		echo "what drive letter and partition? EX: /dev/sda2"
+		read f2fspartionformat
+		mkfs.f2fs -f -l root -O extra_attr,inode_checksum,sb_checksum,compression $f2fspartionformat
+        ;;
+        2)# #
+        #
+        ;;
+        3)##
+        #
+        ;;
+        4)#
+        #
+        ;;
+        5)#
+        #
+        ;;
+        88)#Return to main menu
+        ./archinstall.sh
+        ;;
+        99)
+        poweroff
+        ;;
+      esac
+
+
+;;
+  3)
+
+      #Install Arch Menu
+      echo "
+      ${yellow}Install Arch Menu${AO}
+      1. #
+      2. #
+      3. #
+      88. Return to Main Menu
+      99. Shutdown"
+      read installarch
+      case $installarch in
+        1)#
+        #
+        ;;
+        2)#
+        #
+        ;;
+        3)#
+        #
+        ;;
+        88)#return to main MENU
+        ./archinstall.sh;;
+        99)
+        poweroff;;
+      esac
+
+;;
+  4)
+
+      #Arch-chroot/postinstall MENU
+      echo "
+      ${yellow}Arch-chroot/postinstall MENU${AO}
+      1. #
+      2. #
+      3. #
+      4. #
+      5. #
+      88. Return to Main Menu
+      99. Shutdown"
+      read postinstall
+      case $postinstall in
+        1)#
+       #
+        ;;
+        2)#
+        #
+        ;;
+        3)#
+        #
+        ;;
+        4)#
+        #
+        ;;
+        5)#
+        #
+        ;;
+        88)#return to main menu
+        ./archinstall.sh;;
+        99)#Shutdown
+        poweroff;;
+      esac
+
+;;
+  5)
+
+      #
+	  echo "
+      ${yellow}#${AO}
+      1. #
+      "
+      read placeholder
+      case $placeholder in
+        1)
+        ;;
+        2)
+        ;;
+        3)
+        ;;
+        88)
+        ./archinstall.sh;;
+        99)
+        poweroff;;
+      esac
+
+
+;;
+  88)
+Exit
+;;
+  99)
+poweroff
+;;
+esac
